acbee1c6c039288140c3bb551fde65ad029e8369
Niklas Solomon <niklassolomon@gmail.com>
2022-08-14 19:45:31 -0700

    Write to file function, question constant, done with tutor

--- index.js
+++ index.js
@@ -1,13 +1,57 @@
+
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    //questions go here
+    {
+        type: 'input',
+        message: 'What is the title?',
+        name: 'title',
+      },
+      {
+        type: 'input',
+        message: 'What is the description?',
+        name: 'description',
+      },
+      {
+        type: 'list',
+        message: 'What is the license?',
+        name: 'license',
+        choices: ['Apache', 'Boost']
+      },
+];
 
+// writeToFile function
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+      });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+  .prompt(questions)
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    console.log(answers)
+   writeToFile('./output/README.md', generateMarkdown(answers))
+  })
+//   .catch((error) => {
+//     if (error.isTtyError) {
+//       // Prompt couldn't be rendered in the current environment
+//     } else {
+//       // Something else went wrong
+//     }
+//   });
+}
+
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -10,11 +10,22 @@ function renderLicenseLink(license) {}
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
+function renderTitle(title) {
+  return `# ${title}`;
+}
+
+// data.title
+// data.description
+// data.authorName
+
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${renderTitle(data.title)}
 
+  # DESCRIPTION 
+  ${data.description}
 `;
 }
 
 module.exports = generateMarkdown;
+
